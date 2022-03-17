2429c9983ef4dc98cdec8f96f9657faf02ab7880
tristanfrn <tfarnaeu@gmail.com>
2022-03-17 11:17:48 +0100

    new version

--- pixel.js
+++ pixel.js
@@ -1,1 +1,216 @@
-!function(n){var e={};function t(r){if(e[r])return e[r].exports;var l=e[r]={i:r,l:!1,exports:{}};return n[r].call(l.exports,l,l.exports,t),l.l=!0,l.exports}t.m=n,t.c=e,t.d=function(n,e,r){t.o(n,e)||Object.defineProperty(n,e,{enumerable:!0,get:r})},t.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},t.t=function(n,e){if(1&e&&(n=t(n)),8&e)return n;if(4&e&&"object"==typeof n&&n&&n.__esModule)return n;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:n}),2&e&&"string"!=typeof n)for(var l in n)t.d(r,l,function(e){return n[e]}.bind(null,l));return r},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},t.p="/",t(t.s=6)}({6:function(n,e,t){n.exports=t("N7UG")},N7UG:function(n,e){function t(n,e,t){var r="";if(t){var l=new Date;l.setTime(l.getTime()+24*t*60*60*1e3),r="; expires="+l.toUTCString()}document.cookie=n+"="+(e||"")+r+"; path=/"}function r(n){for(var e=n+"=",t=document.cookie.split(";"),r=0;r<t.length;r++){for(var l=t[r];" "==l.charAt(0);)l=l.substring(1,l.length);if(0==l.indexOf(e))return l.substring(e.length,l.length)}return null}function l(n,e,t,r){var l="https://api.manuscry.com/",i=null;if("score"==n&&(i=l+"p-"+t+"-"+r+"-score-"+e+".jpg"),"event"==n&&(i=l+"p-"+t+"-"+r+"-event-"+e+".jpg"),null!=i){var u=document.createElement("img");u.src=i,u.setAttribute("width",1),u.setAttribute("height",1),u.style.width="1px",u.style.height="1px",u.style.opacity="0",document.getElementById("mpixelimg").appendChild(u)}}var i=function(n){if(""==n)return{};for(var e={},t=0;t<n.length;++t){var r=n[t].split("=",2);1==r.length?e[r[0]]="":e[r[0]]=decodeURIComponent(r[1].replace(/\+/g," "))}return e}(window.location.search.substr(1).split("&")),u=void 0!==i.minstance&&null!==i.minstance?i.minstance:null,o=void 0!==i.mcampaign&&null!==i.mcampaign?i.mcampaign:null;null!=u&&t("minstance",u,7),null!=o&&t("mcampaign",o,7);var a=r("minstance");null==u&&null!=a&&(u=a);var c,s=r("mcampaign");if(null==o&&null!=s&&(o=s),null!=(c=null!=(c=document.querySelector("#mpixelsettings"))&&null!=c?c.innerHTML:null))try{c=JSON.parse(c)}catch(n){c=null}if(null!=u&&null!=o&&null!=c)for(var p in c.actions)l(c.actions[p].action,c.actions[p].data,o,u)}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/pixel.js":
+/*!*******************************!*\
+  !*** ./resources/js/pixel.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// Cookies functions
+function mSetCookie(name, value, days) {
+  var expires = "";
+
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function mGetCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1, c.length);
+    }
+
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+
+  return null;
+}
+
+function mEraseCookie(name) {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+} // Tag functions
+
+
+function mAddPixelImg(action, data, mcampaign, minstance) {
+  var mbaseurl = "https://api.manuscry.com/";
+  var mtagsrc = null;
+  if (action == "score") mtagsrc = mbaseurl + "p-" + mcampaign + "-" + minstance + "-score-" + data + ".jpg";
+  if (action == "event") mtagsrc = mbaseurl + "p-" + mcampaign + "-" + minstance + "-event-" + data + ".jpg";
+
+  if (mtagsrc != null) {
+    var img = document.createElement("img");
+    img.src = mtagsrc;
+    img.setAttribute('width', 1);
+    img.setAttribute('height', 1);
+    img.style.width = "1px";
+    img.style.height = "1px";
+    img.style.opacity = "0";
+    var mtagcontainer = document.getElementById("mpixelimg");
+    mtagcontainer.appendChild(img);
+  }
+} // Get querystring
+
+
+var qs = function (a) {
+  if (a == "") return {};
+  var b = {};
+
+  for (var i = 0; i < a.length; ++i) {
+    var p = a[i].split('=', 2);
+    if (p.length == 1) b[p[0]] = "";else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+
+  return b;
+}(window.location.search.substr(1).split('&')); // Get identifier and campagin
+
+
+var minstance = qs['minstance'] !== undefined && qs['minstance'] !== null ? qs['minstance'] : null;
+var mcampaign = qs['mcampaign'] !== undefined && qs['mcampaign'] !== null ? qs['mcampaign'] : null;
+if (minstance != null) mSetCookie('minstance', minstance, 7);
+if (mcampaign != null) mSetCookie('mcampaign', mcampaign, 7);
+var minstance_from_cookies = mGetCookie('minstance');
+if (minstance == null && minstance_from_cookies != null) minstance = minstance_from_cookies;
+var mcampaign_from_cookies = mGetCookie('mcampaign');
+if (mcampaign == null && mcampaign_from_cookies != null) mcampaign = mcampaign_from_cookies; // Get configuration
+
+var mconfig = document.querySelector('#mpixelsettings');
+var mconfig = mconfig != null && mconfig != undefined ? mconfig.innerHTML : null;
+
+if (mconfig != null) {
+  try {
+    mconfig = JSON.parse(mconfig);
+  } catch (e) {
+    mconfig = null;
+  }
+} // If campaign and identifier is present
+
+
+if (minstance != null && mcampaign != null && mconfig != null) {
+  for (var i in mconfig.actions) {
+    mAddPixelImg(mconfig.actions[i].action, mconfig.actions[i].data, mcampaign, minstance);
+  }
+} // Export function
+
+
+window.ManuscryTrack = function (action, data) {
+  if (action != 'score' && action != 'event') {
+    console.log('ManuscryTrack - Not a valid event type');
+    return false;
+  } else {
+    mAddPixelImg(action, data, mcampaign, minstance);
+    return true;
+  }
+};
+
+/***/ }),
+
+/***/ 6:
+/*!*************************************!*\
+  !*** multi ./resources/js/pixel.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/tristan/Sites/manuscry/app/resources/js/pixel.js */"./resources/js/pixel.js");
+
+
+/***/ })
+
+/******/ });
